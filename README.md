86e817200971ff348bc419c236f9bad447d2b01b
Levis <lfonsoh@terpmail.umd.edu>
2024-05-19 06:53:28 -0400

    v2

--- README.md
+++
@@ -1,2 +0,0 @@
-# inst377-project-repo
-INST377 group project vercel repository
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const supabaseClient = require('@supabase/supabase-js')
+const bodyParser = require('body-parser')
 const express = require('express')
 
 const app = express()
 const port = 3000
-app.use(bodyparser.json())
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/public'))
 
 const supabaseUrl = 'https://psqapgmrgvpnvhpypzhj.supabase.co'
@@ -12,7 +13,7 @@ const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey)
 
 app.get('/contacts', async (req, res) => {
     //res.sendFile('public/377-final-homePage.html', {root: __dirname})
-    //console.log('Attempting to GET all contacts')
+    console.log('Attempting to GET all contacts')
 
     const {data, error} = await supabase
         .from('contacts')
@@ -24,15 +25,16 @@ app.get('/contacts', async (req, res) => {
     } else {
         res.send(data)
     }
-    //console.log('Data:'. data)
-    //console.log('Error:'. error)
+    console.log('Data:'. data)
+    console.log('Error:'. error)
 })
 
+
 app.post('/customers', async(req, res) => {
-    console.log('Adding Customers')
+    console.log('Adding Customer')
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
-    var description = req.body.description
+    var description = req.body.description;
 
     const {data, error} = await supabase
         .from('customers')
@@ -40,13 +42,14 @@ app.post('/customers', async(req, res) => {
         .select()
     
     if(error){
-         console.log('error')
+        console.log('error')
         res.send(error)
     } else {
         res.send(data)
     }
 
 })
+
 
 app.listen(port, () => {
     console.log('APP IS ALIVEEE')
